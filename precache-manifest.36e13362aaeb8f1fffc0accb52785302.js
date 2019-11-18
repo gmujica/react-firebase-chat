28130a388e7389dd281920004ec7e97ba9107a78
gmujica <gregorymujica1993@gmail.com>
2019-11-17 19:13:42 -0500

    Updates

---
+++ precache-manifest.36e13362aaeb8f1fffc0accb52785302.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73222ae3e9f0a7ceb85cf8bd8ae4be22",
+    "url": "/react-firebase-chat/index.html"
+  },
+  {
+    "revision": "8ebac50b88b2ac23d6d3",
+    "url": "/react-firebase-chat/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2bb18b398445d1d345bb",
+    "url": "/react-firebase-chat/static/js/2.c8af360b.chunk.js"
+  },
+  {
+    "revision": "8ebac50b88b2ac23d6d3",
+    "url": "/react-firebase-chat/static/js/main.c339abb9.chunk.js"
+  },
+  {
+    "revision": "9f384d9f445fae476605",
+    "url": "/react-firebase-chat/static/js/runtime-main.a873cf24.js"
+  }
+]);
